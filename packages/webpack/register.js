f9db7aa4d30528b14d6d45745e96bdd7294dd837
Petr Juna <petr.juna@gmail.com>
2018-11-20 10:52:55 +0100

    coverage and tests

---
+++ packages/webpack/register.js
@@ -0,0 +1,5 @@
+require('@babel/register')({
+  // Not possible to configure in .babelrc
+  // https://github.com/babel/babel/issues/8652
+  extensions: ['.ts'],
+});
--- packages/webpack/src/parts/extensions/spec.ts
+++ packages/webpack/src/parts/extensions/spec.ts
@@ -1,6 +1,18 @@
 import ava from 'ava';
 import { extensions } from './part';
 
+ava('should be possible to have default value', (t) => {
+  const expected = {
+    resolve: {
+      extensions: ['.js'],
+    },
+  };
+
+  const result = extensions();
+
+  t.deepEqual(result, expected);
+});
+
 ava('should be possible to customize entry value', (t) => {
   const expected = {
     resolve: {
