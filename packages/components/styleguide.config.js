b6d337c72e2fd349c074243715f9fff5abc5262c
Petr Juna <petr.juna@gmail.com>
2018-11-26 16:33:10 +0100

    styleguidist heroku

--- packages/components/styleguide.config.js
+++ packages/components/styleguide.config.js
@@ -3,4 +3,6 @@ const { typescriptCss } = require('@pyxis/webpack/typescriptCss.config');
 module.exports = {
   webpackConfig: typescriptCss({ target: 'web', mode: 'development' }),
   components: 'src/components/**/*.tsx',
+  serverPort: process.env.PORT || 6060,
+  serverHost: '0.0.0.0',
 };
