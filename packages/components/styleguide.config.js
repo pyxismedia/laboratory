b0847ff099be9ae35195e8f41aa823368584bd27
Petr Juna <petr.juna@gmail.com>
2018-11-27 14:00:15 +0100

    styleguidist heroku

--- packages/components/styleguide.config.js
+++ packages/components/styleguide.config.js
@@ -4,5 +4,4 @@ module.exports = {
   webpackConfig: typescriptCss({ target: 'web', mode: 'development' }),
   components: 'src/components/**/*.tsx',
   serverPort: process.env.PORT || 6060,
-  serverHost: '0.0.0.0',
 };
