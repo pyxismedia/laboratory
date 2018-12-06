2935ff56b44f4b5dfb6eea5f7efd1dbf3fdcd225
Petr Juna <petr.juna@gmail.com>
2018-12-06 22:48:18 +0100

    update

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -1,8 +1,10 @@
 const { typescriptCss } = require('@pyxis/webpack/build/configs/typescriptCss');
+const externals = require('webpack-node-externals');
 
 module.exports = typescriptCss({
   entry: {
     'construction': './src/components/construction',
     'logo': './src/components/logo',
   },
+  externals: [externals()],
 });
