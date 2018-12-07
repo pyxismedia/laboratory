827eadb222ecdee674fce0dd1b3797d08a7110c7
Petr Juna <petr.juna@gmail.com>
2018-12-07 14:50:17 +0100

    v0.0.44

--- packages/components/src/components/construction/component.tsx
+++ packages/components/src/components/construction/component.tsx
@@ -9,13 +9,37 @@ export interface ConstructionProps {
   logo: string;
   /** background color */
   backgroundColor: string
+  /** contact email address */
+  email: string;
+  /** contact phone number */
+  phone: number;
+  /** title that represent big text */
+  title: string;
+  /** subtitle represent slogan or */
+  subtitle: string;
 }
 
 /**
  * Construction component.
  */
-export const Construction = ({ logo, backgroundColor }: ConstructionProps) => (
+export const Construction = ({ logo, backgroundColor, email, phone, title, subtitle }: ConstructionProps) => (
   <div className="construction" style={{ backgroundColor }}>
-    <img className="construction__image" src={logo} alt="" />
+    <div className="construction__container">
+      <>
+      {logo ?
+        <img className="construction__image" src={logo} alt="" /> :
+        <>
+          <div className="construction__title">{title}</div>
+          <div className="construction__subtitle">{subtitle}</div>
+        </>
+      }
+      </>
+      <div className="contruction__contact">
+        <span className="contruction__channel">
+          <a className="construction__email" href={`mailto:${email}`}>{email}</a>
+        </span>
+        <span className="contruction__channel">{phone}</span>
+      </div>
+    </div>
   </div>
 );
