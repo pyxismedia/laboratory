b43dc21fb037a947e9641c61896f3a5a2d535bcd
Petr Juna <petr.juna@gmail.com>
2019-01-28 17:01:47 +0100

    fix(host): hOST for heroku added
    affects: @pyxis/keystone

--- packages/keystone/app/constants.ts
+++ packages/keystone/app/constants.ts
@@ -3,3 +3,4 @@ export const PRODUCTION = 'production';
 export const ENVIRONMENT = process.env.NODE_ENV || 'production';
 export const PORT = process.env.PORT || 3002;
 export const MONGODB = process.env.MONGODB_URI || 'mongodb://localhost/pyxis';
+export const HOST = process.env.HOST || '0.0.0.0';
