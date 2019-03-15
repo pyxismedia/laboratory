7a8b9af346e199cb3a97f615131062fd5dfacd48
Petr Juna <petr.juna@gmail.com>
2019-03-15 20:57:57 +0100

    fix(build): fixing build for travis
    affects: @pyxis/theme, @pyxis/components, @pyxis/nest
    
    ALl builds were failing this ws inital fix to make them work

--- packages/lib/components/webpack.config.js
+++ packages/lib/components/webpack.config.js
@@ -3,13 +3,13 @@ const { sass } = require('@pyxis/webpack/build/parts/sass');
 const externals = require('webpack-node-externals');
 const merge = require('webpack-merge');
 
-module.exports = merge(
+module.exports = merge.smart(
   typescript({
     entry: {
       'construction': './src/components/templates/construction',
       'logo': './src/components/atoms/svg',
     },
     externals: [externals()],
-  },
+  }),
   sass(),
-));
+);
