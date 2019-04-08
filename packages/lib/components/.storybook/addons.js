48f539e7008f4372e1066cd189b3091e58c1ff10
Petr <petr.juna@gmail.com>
2019-04-08 10:49:33 +0200

    incomplete

--- packages/lib/components/.storybook/addons.js
+++ packages/lib/components/.storybook/addons.js
@@ -1,5 +1,4 @@
 import 'storybook-readme/register';
-import '@storybook/addon-options/register';
 import '@storybook/addon-knobs/register';
 import './addons/sass-theme/register';
 import '@storybook/addon-actions/register';
--- packages/lib/components/.storybook/webpack.config.js
+++ packages/lib/components/.storybook/webpack.config.js
@@ -5,7 +5,7 @@ const webpackMerge = require('webpack-merge');
 const { assign } = Object;
 
 // Export a function. Accept the base config as the only param.
-module.exports = (storybookBaseConfig, configType, defaultConfig) => {
+module.exports = ({ config: defaultConfig }) => {
   // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
   // You can change the configuration based on that.
   // 'PRODUCTION' is used when building the static version of storybook.
