7d546a51f116b67b7b6d59021444a0137314e280
Petr Juna <petr.juna@gmail.com>
2019-05-05 15:50:52 +0200

    chore(product details): initial files for product detail
    affects: @pyxis/components

---
+++ packages/lib/components/src/components/templates/product-detail/components.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.scss';
+
+export const ProductDetail = () => (
+  <div>
+    <div className={styles.image}>test</div>
+  </div>
+);
---
+++ packages/lib/components/src/components/templates/product-detail/story.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { ProductDetail } from './components';
+
+storiesOf('Templates/ProductDetail', module).add('default', () => (
+  <ProductDetail />
+));
