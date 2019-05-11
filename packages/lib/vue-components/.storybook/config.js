8e1d228ac540d63993eadbfdc95b89069acd4f89
paragdiwan <paragdiwan@gmail.com>
2019-05-11 08:25:53 +0530

    revert: reverting last change did for vuejs configuration
    affects: @pyxis/vue-components

--- packages/lib/vue-components/.storybook/config.js
+++ packages/lib/vue-components/.storybook/config.js
@@ -4,7 +4,7 @@ import { addReadme } from 'storybook-readme/vue';
 addDecorator(addReadme);
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../', true, /\.stories\.js$/);
+const req = require.context('../src', true, /story\.tsx?$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
