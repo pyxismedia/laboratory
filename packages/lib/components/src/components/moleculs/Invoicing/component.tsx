eb3985c751a97fe69291b72d02c4860aacfa286f
Petr Juna <petr.juna@gmail.com>
2019-05-28 20:29:56 +0200

    fields

--- packages/lib/components/src/components/moleculs/Invoicing/component.tsx
+++ packages/lib/components/src/components/moleculs/Invoicing/component.tsx
@@ -1,49 +1,70 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { City, Country } from './types';
 
-export const Invoicing = () => (
+interface InvoicingProps {
+  forname: string;
+  surname: string;
+  company: string;
+  vat: string;
+  street: string;
+  streetNo: number;
+  postcode: string;
+  cities: City[];
+  countries: Country[];
+}
+
+export const Invoicing: FunctionComponent<InvoicingProps> = ({ forname, cities, countries, surname, company, vat, street, streetNo, postcode }) => (
   <>
     <div className="form-row">
       <div className="col-6 mb-3">
-        <label for="">Forname</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
+        <label htmlFor="">Forname</label>
+        <input type="text" className="form-control" name="forname" id="" placeholder="" value={forname} required />
       </div>
       <div className="col-6 mb-3">
-        <label for="">Surname</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
-      </div>
-    </div>
-    <div className="form-row">
-      <div className="col-6 mb-3">
-        <label for="">Company</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
-      </div>
-      <div className="col-6 mb-3">
-        <label for="">VAT</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
+        <label htmlFor="">Surname</label>
+        <input type="text" className="form-control" id="surname" placeholder="" value={surname} required />
       </div>
     </div>
     <div className="form-row">
       <div className="col-6 mb-3">
-        <label for="">Street</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
+        <label htmlFor="">Company</label>
+        <input type="text" className="form-control" id="company" placeholder="" value={company} required />
+      </div>
+      <div className="col-6 mb-3">
+        <label htmlFor="">VAT</label>
+        <input type="text" className="form-control" id="vat" placeholder="" value={vat} required />
+      </div>
+    </div>
+    <div className="form-row">
+      <div className="col-6 mb-3">
+        <label htmlFor="">Street</label>
+        <input type="text" className="form-control" id="street" placeholder="" value={street} required />
       </div>
       <div className="col-2 mb-3">
-        <label for="">Street no.</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
+        <label htmlFor="">Street no.</label>
+        <input type="text" className="form-control" id="streetNo" placeholder="" value={streetNo} required />
       </div>
       <div className="col-4 mb-3">
-        <label for="">Street no.</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
+        <label htmlFor="">Postcode</label>
+        <input type="text" className="form-control" id="postcode" placeholder="" value={postcode} required />
       </div>
     </div>
     <div className="form-row">
       <div className="col-6 mb-3">
-        <label for="">City</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
+        <label htmlFor="city">City</label>
+        <select className="custom-select" id="city">
+          {cities.map((city: City) => (
+            <option key={city.id} value={city.value}>{city.title}</option>
+          ))}
+        </select>
       </div>
       <div className="col-6 mb-3">
-        <label for="">Country</label>
-        <input type="text" className="form-control" id="" placeholder="" value="" required />
+        <label htmlFor="country">Country</label>
+        <select className="custom-select" id="country">
+          {countries.map((country: Country) => (
+            <option key={country.id} value={country.value}>{country.title}</option>
+          ))}
+        </select>
       </div>
     </div>
   </>
--- packages/lib/components/src/components/moleculs/Invoicing/story.tsx
+++ packages/lib/components/src/components/moleculs/Invoicing/story.tsx
@@ -2,7 +2,41 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Invoicing } from './component';
 
+const props = {
+  forname: 'Karel',
+  surname: 'Vomacka',
+  company: 'Neco LTD',
+  vat: '123456',
+  street: 'Some Street',
+  streetNo: 5,
+  postcode: '12345',
+  countries: [
+    {
+      id: 'czechRepublic',
+      title: 'Czech Republic',
+      value: 'CZ'
+    },
+    {
+      id: 'germany',
+      title: 'Germany',
+      value: 'DE'
+    }
+  ],
+  cities: [
+    {
+      id: 'prague',
+      title: 'Prague',                        
+      value: 'prague'
+    },
+    {
+      id: 'berlin',
+      title: 'Berlin',
+      value: 'berlin'
+    }
+  ]
+}
+
 storiesOf('Moleculs/Invoicing', module)
   .add('default', () => (
-    <Invoicing /> 
+    <Invoicing {...props} /> 
   ));
